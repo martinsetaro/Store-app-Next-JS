6f5650565b3a2167934f9360cc025f5b666db28d
martinsetaro <lordsesony@gmail.com>
2022-10-09 01:31:11 -0300

    agregado de imagen

--- pages/Monedas.js
+++ pages/Monedas.js
@@ -1,7 +1,8 @@
 
 import { useContext } from "react";
 import {contexto} from "../pages/AppContext.js";
-
+import dolar from '../public/img/dolar.png';
+import Image from "next/future/image.js";
 import Swal from 'sweetalert2'
 
 
@@ -30,7 +31,7 @@ sumarMonedas(5000);
    
     <div className="w-1/2 m-auto h-24 shadow-md flex justify-center items-center gap-3 rounded-md">
         
-       <h2 className="text-2xl">Monedas<span>${monedas}</span></h2>
+       <Image src={dolar} alt="moneda" className="w-10 h-10"/><h2 className="text-2xl">Monedas<span>${monedas}</span></h2>
         <button onClick={agregarMonedas} className="text-4xl rounded-full bg-orange-500 w-10 h-10 text-white hover:bg-slate-400 ">+</button>
        
     </div>
